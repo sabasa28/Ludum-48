8af5abb3e57b6c7acb77af1ba7a2975fc5b57993
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2021-04-26 18:52:06 -0300

    Agregada escena de main menu, falta configurarla

---
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMainMenu : MonoBehaviour
+{
+    public GameObject credits;
+    public GameObject controls;
+    public void LoadGameplay()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Closed");
+    }
+    public void ShowCredits()
+    {
+        credits.SetActive(true);
+    }
+    public void HideCredits()
+    { 
+        credits.SetActive(false);
+    }
+    public void ShowControls()
+    {
+        controls.SetActive(true);
+    }
+    public void HideControls()
+    {
+        controls.SetActive(false);
+    }
+}
